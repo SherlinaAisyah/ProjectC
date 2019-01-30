9263c0db5792e56c38d7197f7a18e3fa39448eed
SherlinaAisyah <sherina360@gmail.com>
2019-01-30 23:46:47 +0700

    Version 1.0.0.0

---
+++ PROGRAM SISTEM BILANGAN/PEMBAGIAN BINER/PEMBAGIAN BINER/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PEMBAGIAN_BINER
+{
+    class Program
+    {
+        int mB2D(int BIN)
+        {
+            int DEC = 0, X = 0;
+            while (BIN > 0)
+            {
+                DEC += (int)(BIN % 10 * Math.Pow(2, X++));
+                BIN /= 10;
+            }
+            return DEC;
+        }
+        int mD2B(int DEC)
+        {
+            int BIN = 0, X = 0;
+            while (DEC > 0)
+            {
+                BIN += (int)(DEC % 2 * Math.Pow(10, X++));
+                DEC /= 2;
+            }
+            return BIN;
+        }
+        static void Main(string[] args)
+        {
+            int BIN, BIN1, BIN2, DEC, DEC1, DEC2;
+            Console.WriteLine("     PEMBAGIAN BINER ");
+            Console.WriteLine("  Oleh : Sherli - XT3/33");
+            Console.Write("\n     BIl. BIN 1  = ");
+            BIN1 = int.Parse(Console.ReadLine());
+            Console.Write("\n     Bil. BIN 2  = ");
+            BIN2 = int.Parse(Console.ReadLine());
+
+
+            Program n1 = new Program();
+            DEC1 = n1.mB2D(BIN1);
+            DEC2 = n1.mB2D(BIN2);
+            DEC = DEC1 / DEC2;
+
+            Program n2 = new Program();
+            BIN = n2.mD2B(DEC);
+
+            Console.Write("\n     BIN1 / BIN2 = " + BIN);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ PROGRAM SISTEM BILANGAN/PENJUMLAHAN OCTAL/PENJUMLAHAN OCTAL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace PENJUMLAHAN_OCTAL
+{
+    class Program
+    {
+             int mB2D(int OCT)
+        {   int DEC = 0, X = 0;
+            while (OCT > 0)
+            {
+            DEC += (int) (OCT % 10 * Math.Pow(8, X++));
+            OCT /= 10;
+            }
+            return DEC;
+        }
+        int mD2B(int DEC)
+            {
+                int OCT = 0, X = 0;
+                while(DEC > 0)
+            {
+        OCT += (int)(DEC % 8 * Math.Pow(10, X++));
+        DEC /= 8;               }
+            return OCT;
+             }
+        static void Main(string[] args)
+        {
+            int OCT, OCT1, OCT2, DEC, DEC1, DEC2;
+            Console.WriteLine("     PENJUMLAHAN OCTAL ");
+            Console.WriteLine("   Oleh : Sherli - XT3/33");
+            Console.Write("\n     BIl. OCT 1  = ");
+            OCT1 = int.Parse(Console.ReadLine());
+            Console.Write("\n     Bil. OCT 2  = ");
+            OCT2 = int.Parse(Console.ReadLine());
+
+            Program n1 = new Program();
+            DEC1 = n1.mB2D(OCT1);
+            DEC2 = n1.mB2D(OCT2);
+            DEC = DEC1 + DEC2;
+
+            Program n2 = new Program();
+            OCT = n2.mD2B(DEC);
+
+            Console.Write("\n   OCT.1 + OCT.2 = " + OCT);
+            Console.ReadKey();
+           
+        }
+    }
+}
+        
